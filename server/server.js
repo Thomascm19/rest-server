624ec10e4faca720a9a13efa490d32ffc7ca32de
Thomas <thomascaycedo19@outlook.com>
2019-04-14 12:13:11 -0500

    Se realizan las peticiones get post put delete

--- server/server.js
+++ server/server.js
@@ -1,10 +1,45 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
  
-app.get('/', function (req, res) {
-  res.json('Hello World')
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', function (req, res) {
+    res.json('get usuario')
 })
- 
-app.listen(3000, () =>{
+app.post('/usuario', function (req, res) {
+
+    let body = req.body;
+
+    if(body.nombre === undefined){
+        res.status(400).json({
+            ok:false,
+            mensaje:'Nombre no valido'
+        })
+    }else{
+        res.json({
+            persona:body
+        })
+    }
+    
+})
+
+app.put('/usuario/:id', function (req, res) {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    })
+})
+app.delete('/usuario', function (req, res) {
+    res.json('delete Usuario')
+})
+
+app.listen(3000, () => {
     console.log('Escuchando puerto 3000')
 })
